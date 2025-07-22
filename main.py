4a20c9f0747738ee96af0401130100b34ac66dd5
Fabiola Sakae <fabiola.sakae@gmail.com>
2025-07-22 20:45:48 -0300

    feat: Atualiza mensagem da aplicacao para V2

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello from CI/CD Pipeline! PROJETO CI/CD com GITHUB ACTIONS - DevSecOps! DOCKER HUB como registry, e ArgoCD para entrega contínua em kUBERNETES local com MINIKUBE"}
+    return {"message": "V2 Hello from CI/CD Pipeline! PROJETO CI/CD com GITHUB ACTIONS - DevSecOps! DOCKER HUB como registry, e ArgoCD para entrega contínua em kUBERNETES local com MINIKUBE"}
